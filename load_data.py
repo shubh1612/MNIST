ac32a9d3e69f7c45749a990bac14c1ef20cb717a
Shubham Singla <25100284+shubh1612@users.noreply.github.com>
2019-02-13 09:46:03 +0530

    Load the Dataset from files

---
+++ load_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import gzip
+import os
+
+def load_mnist(path, kind='train'):
+
+	## Load Data from path specified
+	labels_path = os.path.join(path, '%s-labels-idx1-ubyte.gz'% kind)
+	images_path = os.path.join(path, '%s-images-idx3-ubyte.gz'% kind)
+
+	with gzip.open(labels_path, 'rb') as lbpath:
+		labels = np.frombuffer(lbpath.read(), dtype=np.uint8, offset=8)
+
+	with gzip.open(images_path, 'rb') as imgpath:
+		images = np.frombuffer(imgpath.read(), dtype=np.uint8, offset=16).reshape(len(labels), 784)
+
+	return images, labels
